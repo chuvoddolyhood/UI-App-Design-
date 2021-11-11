8b5be6cffce111ec8804d3b21460380509616ebf
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2021-11-11 16:18:08 +0700

    design explore product for interior shop

---
+++ interiorShop/app/src/main/java/com/example/interiorshop/Explore_2_Activity.java
@@ -0,0 +1,14 @@
+package com.example.interiorshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Explore_2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_2_);
+    }
+}
